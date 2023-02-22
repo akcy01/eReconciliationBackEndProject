1452c01e9941e45c92ce952a427a74d302c9b35a
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2023-02-22 23:33:55 +0300

    CurrencyAccount entity is completed.

--- Entities/Concrete/Currency.cs
+++ Entities/Concrete/Currency.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    internal class Currency
+    public class Currency : IEntity
     {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+
     }
 }
