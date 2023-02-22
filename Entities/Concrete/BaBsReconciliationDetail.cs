3d380db4f1c6d52ced143495f36e0b8b9f1e91a1
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2023-02-22 23:22:05 +0300

    BaBsReconciliationDetail entity is completed.

--- Entities/Concrete/BaBsReconciliationDetail.cs
+++ Entities/Concrete/BaBsReconciliationDetail.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    internal class BaBsReconciliationDetail
+    public class BaBsReconciliationDetail : IEntity
     {
+        public int Id { get; set; } 
+        public int BaBsReconciliationId { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+
     }
 }
