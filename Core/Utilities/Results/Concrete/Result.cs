b722d3b37a80a4c728f0bbbcb2cd16dae2173ae8
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2023-02-24 20:17:44 +0300

    Utilities completed.

---
+++ Core/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,25 @@
+﻿using Core.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results.Concrete
+{
+    public class Result : IResult
+    {
+        /* Geriye sonuç döndürdüğümüzde ya sadece successi ya da messageyi veya ikisini bir döndürebiliriz diye iki adet constructor oluşturduk. */
+        public Result(bool success)
+        {
+            Success = success;
+        }
+        public Result(bool success, string message) : this(success)
+        {
+            Message = message;
+        }
+
+        public bool Success { get; }
+        public string Message { get; }
+    }
+}
