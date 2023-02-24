4ce5645c459e054d2dad83d3d971509150ab1a4b
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2023-02-24 09:58:23 +0300

    IEntityRepository completed.

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);//İçerisinde sorgu yazabilmek için expression ekledk
+        T Get(Expression<Func<T, bool>> filter);
+    }
+}
