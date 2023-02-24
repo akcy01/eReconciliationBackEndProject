dec778ebcf38b72a86275701422eb7463612ea22
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2023-02-24 10:55:18 +0300

    Interfaces are completed.

--- DataAccess/Abstract/IAccountReconciliationDetailDal.cs
+++ DataAccess/Abstract/IAccountReconciliationDetailDal.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface ICompanyDal : IEntityRepository<Company>
+    public interface IAccountReconciliationDetailDal : IEntityRepository<AccountReconciliationDetail>
     {
 
     }
