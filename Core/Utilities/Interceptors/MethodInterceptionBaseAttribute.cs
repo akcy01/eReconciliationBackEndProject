14cfc4948e3bcad38c6956f51ad69609530506e0
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2023-03-16 14:06:25 +0300

    MethodInterception is completed.

---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Castle.Core.Interceptor;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; } // Sıra verebilmek adına hazırlık
+        public virtual void Intercept(IInvocation invocation)
+        {
+            
+        }
+    }
+}
