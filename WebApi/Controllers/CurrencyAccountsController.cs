c1836810291773573998e4ee46256888e020bdfe
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2023-03-22 11:37:56 +0300

    Add excel file proccess completed.

--- WebApi/Controllers/CurrencyAccountsController.cs
+++ WebApi/Controllers/CurrencyAccountsController.cs
@@ -17,26 +17,25 @@ namespace WebApi.Controllers
         }
 
         [HttpPost("addFromExcel")]
-        public IActionResult AddFromExcel(CurrencyAccountExcelDto currencyAccount)
+        public IActionResult AddFromExcel(IFormFile file, int companyId) //IFormFile sayesinde dosya yükleyebiliyoruz.
         {
-            if(currencyAccount.File.Length > 0)
+            if(file.Length > 0)
             {
                 var fileName = Guid.NewGuid().ToString() + ".xlsx";
-                var filePath = $"({Directory.GetCurrentDirectory()}/Content/{fileName})";
+                var filePath = $"{Directory.GetCurrentDirectory()}/Content/{fileName}";
                 using (FileStream stream = System.IO.File.Create(filePath))
                 {
-                    currencyAccount.File.CopyTo(stream);
+                    file.CopyTo(stream);
                     stream.Flush();
                 }
 
-                var result = _currencyAccountService.AddToExcel(filePath);
+                var result = _currencyAccountService.AddToExcel(filePath, companyId);
                 if (result.Success)
                 {
                     return Ok(result);
                 }
                 return BadRequest(result.Message);
             }
-
             return BadRequest("Dosya seçimi yapmadınız");
         }
 
